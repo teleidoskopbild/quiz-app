4c4b222470e100c0cd63cb860d1601294fe5a308
salehAlkarabubi <karabubi66@yahoo.com>
2024-10-02 16:24:00 +0200

    add video

--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -1,31 +1,26 @@
 import { quizQuestions } from "../data/questions.js";
+import "./ResultsPage.css";
 
 const ResultPage = ({ correctAnswers, setPage, setCorrectAnswers }) => {
-  // This function will reload the page to allow the user to try again
-
   return (
     <div className="result-page">
       <h1>Result</h1>
       <p>
         Your score: {correctAnswers} out of {quizQuestions.length}
       </p>
+
       {/* Conditional Rendering */}
       {correctAnswers >= quizQuestions.length ? (
-        <>
-          <p>This is great</p> {/* Show "This is great" message */}
-        </>
+        <p>This is great!</p> /* Show "This is great" message */
       ) : (
-        <>
-          <button
-            onClick={() => {
-              setPage("quiz");
-              setCorrectAnswers(0);
-            }}
-          >
-            Try Again
-          </button>{" "}
-          {/* Show "Try Again" button */}
-        </>
+        <button
+          onClick={() => {
+            setPage("quiz");
+            setCorrectAnswers(0);
+          }}
+        >
+          Try Again
+        </button> /* Show "Try Again" button */
       )}
     </div>
   );
